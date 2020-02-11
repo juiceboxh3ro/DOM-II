7cfbfa7c8ec1285fd1a7292224971c0b97bfcfc3
Jesse <fkng3rmz@gmail.com>
2020-02-11 16:06:15 -0500

    stopped Link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+document.querySelectorAll('.nav-link').forEach(el => {
+  el.addEventListener('click', (e)=> {
+    event.preventDefault();
+    console.log("no.");
+  })
+})
+
+const funBus = document.querySelector('.intro img');
+
+funBus.addEventListener('mouseover', ()=> {
+  funBus.style.transform = 'scale(1.1)'
+})
